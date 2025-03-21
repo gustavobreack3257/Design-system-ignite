09c5ac1df1089684e0174586b3c025271f119d93
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2025-03-21 08:35:49 -0300

    feat: The Design systems base: React package config

--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -1,4 +1,38 @@
 "use strict";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/index.tsx
+var index_exports = {};
+__export(index_exports, {
+  App: () => App
+});
+module.exports = __toCommonJS(index_exports);
 
 // ../tokens/dist/index.mjs
 var colors = {
@@ -18,5 +52,12 @@ var colors = {
   ignite900: "#00291D"
 };
 
-// src/index.ts
-console.log(colors);
+// src/index.tsx
+var import_react = __toESM(require("react"));
+function App() {
+  return /* @__PURE__ */ import_react.default.createElement("h1", { style: { color: colors.ignite300 } }, "Hello world");
+}
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  App
+});
